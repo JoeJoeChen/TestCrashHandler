cd3eca684dc65ca4f572d13ea5346c7928cc7b1f
JoeChen <zhucl@yahoo.cn>
2013-03-12 10:18:41 +0800

    demo

---
+++ src/com/example/testcrashhandler/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testcrashhandler.activity;
+
+import com.example.testcrashhandler.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String s = null;
+//        s.equals("s");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
---
+++ src/com/example/testcrashhandler/common/CrashApplication.java
@@ -0,0 +1,16 @@
+package com.example.testcrashhandler.common;
+
+import android.app.Application;
+
+public class CrashApplication extends Application
+{
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+    }
+
+}
